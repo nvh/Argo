633fbe43285b85a38a0979506060ba3198734cf2
Niels van Hoorn <nvh@nvh.io>
2015-02-10 18:26:49 +0100

    Added JSONResultDecodable protocol

--- Argo/Protocols/JSONDecodable.swift
+++ Argo/Protocols/JSONDecodable.swift
@@ -2,3 +2,8 @@ public protocol JSONDecodable {
   typealias DecodedType = Self
   static func decode(JSONValue) -> DecodedType?
 }
+
+public protocol JSONResultDecodable: JSONDecodable {
+  typealias DecodedResultType = Self
+  class func decodeResult(JSONValue) -> JSONResult<DecodedResultType>
+}
