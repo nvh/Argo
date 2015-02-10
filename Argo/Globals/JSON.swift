692d29b625a38a5614d96f9cf50ed8c60b03ace2
Niels van Hoorn <nvh@nvh.io>
2015-02-10 18:28:14 +0100

    Adding JSONResultDecodable to primitive types

--- Argo/Globals/JSON.swift
+++ Argo/Globals/JSON.swift
@@ -1,5 +1,6 @@
 import Foundation
 
+//MARK: JSONDecodable
 extension String: JSONDecodable {
   public static func decode(j: JSONValue) -> String? {
     return j.value()
@@ -24,6 +25,36 @@ extension Bool: JSONDecodable {
   }
 }
 
+//MARK: JSONResultDecodable
+func makeResultDecodable<A:JSONDecodable>(f: JSONValue -> A?, j: JSONValue) -> JSONResult<A> {
+    return JSONResult(optional: f(j))
+}
+
+extension String: JSONResultDecodable {
+  public static func decodeResult(j: JSONValue) -> JSONResult<String> {
+    return makeResultDecodable(String.decode,j)
+  }
+}
+
+extension Int: JSONResultDecodable {
+  public static func decodeResult(j: JSONValue) -> JSONResult<Int> {
+    return makeResultDecodable(Int.decode,j)
+  }
+}
+
+extension Double: JSONResultDecodable {
+  public static func decodeResult(j: JSONValue) -> JSONResult<Double> {
+    return makeResultDecodable(Double.decode,j)
+  }
+}
+
+extension Bool: JSONDecodable {
+  public static func decodeResult(j: JSONValue) -> JSONResult<Bool> {
+    return makeResultDecodable(Bool.decode,j)
+  }
+}
+
+//MARK: JSONEncodable
 extension Float: JSONDecodable {
   public static func decode(j: JSONValue) -> Float? {
     return j.value()
